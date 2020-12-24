24ed863f0de3481883ce81e4ea2c7cf491645300
brunaoliveira <brunaol678@gmail.com>
2020-12-24 12:21:15 -0300

    examples of return onPressed button

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,42 @@ main() {
 }
 
 class QuizApp extends StatelessWidget {
+  void answer() {
+    print('Question answered!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final questions = [
+      'What is your favourite colour?',
+      'What is your favourite food?',
+      'What is your favourite animal?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Questions'),
           centerTitle: true,
         ),
-        body: Text('Henllo'),
+        body: Column(
+          children: [
+            Text('Henllo'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answer,
+            ),
+            RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: () {
+                  print('Question 2 answered.');
+                }),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => print('Question 3 answered.'),
+            ),
+          ],
+        ),
       ),
       darkTheme: ThemeData(
         accentColor: Colors.pinkAccent,
